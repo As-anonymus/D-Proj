d9a38a3b8ace65118d84dbfc4cc07ef2488af9ba
Aditya Singh <adibetu13@gmail.com>
2023-02-06 11:46:56 +0530

    dele,sidebar

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room,Topic
 from .forms import RoomForm
 # Create your views here.
 
@@ -20,8 +20,14 @@ rooms =[
 
 
 def home(request):
+
+
+
     rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    
+    topics = Topic.objects.all()
+    
+    context = {'rooms':rooms,'topics':topics}
     return render(request, 'base/home.html',context)
 
 
@@ -56,3 +62,10 @@ def updateRoom(request,pk):
 
     context = {'form':form}
     return render(request, 'base/room_form.html',context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html',{'obj':room})
