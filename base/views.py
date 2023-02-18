c45e03ab0cfdf3cec28b3f79485ee5307fc2fbb9
Aditya Singh <adibetu13@gmail.com>
2023-02-18 10:39:05 +0530

    message CRUD

--- base/views.py
+++ base/views.py
@@ -106,6 +106,7 @@ def room(request,pk):
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room',pk=room.id)
     context = {'room':room,'room_messages':room_messages,'participants':participants}
     return render(request, 'base/room.html',context,)
@@ -153,3 +154,16 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html',{'obj':room})
+
+
+
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed')
+        
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html',{'obj':message})
